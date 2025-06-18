c51d80cb7a0dd16f41470c302fbd86e721544545
KIT <nyukkitchin@gmail.com>
2025-06-18 23:09:33 +0800

    Test

---
+++ JavaAssignment/src/main/java/com/mycompany/javaassignment/JavaAssignment.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javaassignment;
+
+/**
+ *
+ * @author KIT
+ */
+public class JavaAssignment {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
